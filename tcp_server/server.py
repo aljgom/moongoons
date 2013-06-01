81cb27a32deddee56b7e5d83d4277545bccf7f74
Sam Liu <sam@ambushnetworks.com>
2013-06-01 00:24:41 -0500

    renames

---
+++ tcp_server/server.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import socket
+import time
+
+# Open and connect TCP socket
+TCP_IP = '192.168.1.1'
+TCP_PORT = 7777
+BUFFER_SIZE = 9216
+YUV_SIZE = 460800
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+# Declare buffers
+image = bytearray(YUV_SIZE)
+packet = bytearray(BUFFER_SIZE)
+
+# Loop indefinitely
+for image_number in range(0, 5):
+
+    START = int(round(time.time() * 1000))
+
+    # Loop 50 times to get single entire image
+    for packet_number in range(0, 50):
+
+        # Receive 9216 bytes of data from server using recv_into
+        bytes_recvd = s.recv_into(packet)
+        total_bytes = bytes_recvd
+
+        for m in range(0, bytes_recvd):
+            index = packet_number * BUFFER_SIZE + m
+            image[index] = packet[m]
+
+        # Make sure all 9216 bytes have been received
+        while (total_bytes < BUFFER_SIZE):
+            packet = bytearray(BUFFER_SIZE-total_bytes)
+            bytes_recvd = s.recv_into(packet)
+            for m in range(0, bytes_recvd):
+                index = packet_number * BUFFER_SIZE + total_bytes + m
+                image[index] = packet[m]
+            total_bytes += bytes_recvd
+
+    print "image %d received" % (image_number)
+
+    print "time is %d" % (int(round(time.time() * 1000))-START)
+
+#   fd = open('image' + str(image_number) + '.yuv', 'wb')
+#   fd.write(image)
+
+
+    ##########################################################
+    # Insert Image Processing Algorithm
+    ##########################################################
+
+    # Output Angle Displacement back to main_motorboard.c
+    #if (len(str(ang_dspl))!=3)
+    #   length = len(str(ang_dspl))
+    #   output = bytearray(3)
+    #   for t in xrange(0,length-1)
+
+
+
+#s.send(MESSAGE)
+
+#s.close()
+print "received image data"
