b86d99970ee8112e439723e995be180737c0d083
Sam Liu <sam@ambushnetworks.com>
2013-06-02 16:31:42 -0500

    refactored server

--- tcp_server/server.py
+++ tcp_server/server.py
@@ -1,113 +1,151 @@
 #!/usr/bin/env python
 
+import cv
+import os
 import socket
 import subprocess
-import os
-import cv
+import sys
 import time
+
 from colortracker import ColorTracker
 
 
-# Open and connect TCP socket
-TCP_IP = '192.168.1.1'
-TCP_PORT = 7777
-BUFFER_SIZE = 9216
-YUV_SIZE = 460800
+class PositionServer():
 
-# Instantiate socket connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+    def __init__(self, ip='192.168.1.1', port=7777, buffer_size=9216, yuv_size=460800, display=False, threshold=10000):
+        # Initialize the environment
+        self.tcp_ip = ip
+        self.tcp_port = port
+        self.buffer_size = buffer_size
+        self.yuv_size = yuv_size
 
-# Declare buffers
-image = bytearray(YUV_SIZE)
-packet = bytearray(BUFFER_SIZE)
+        # Allocate memory for images and packets
+        self.image = bytearray(self.yuv_size)
+        self.packet = bytearray(self.buffer_size)
 
-# Color tracker
-colortracker = ColorTracker()
+        # Instantiate color tracker
+        self.colortracker = ColorTracker()
 
-# Camera display
-cv.NamedWindow("Camera", 1)
-font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.5, 1, 0, 2, 8)
-display_width = 640
-display_height = 480
+        # Display initialization
+        self.display = display
 
-# Loop indefinitely
-image_number = 0
-while True:
+        # Colortracker threshold
+        self.threshold = threshold
 
-    START = int(round(time.time() * 1000))
+        # Camera display
+        if self.display:
+            cv.NamedWindow("Camera", 1)
+            self.cvfont = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.5, 1, 0, 2, 8)
+            self.display_width = 640
+            self.display_height = 480
 
-    # 50 packets for 1 image
-    for packet_number in range(0, 50):
+    def connect(self):
+        # Create socket connection
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.tcp_ip, self.tcp_port))
 
-        # Receive 9216 bytes of data from server
-        bytes_recvd = s.recv_into(packet)
-        total_bytes = bytes_recvd
+    def main_loop(self):
+        image_number = 0
+        while True:
+            start_time = int(round(time.time() * 1000))
 
-        # TODO: optimize using list slicing
-        for m in range(0, bytes_recvd):
-            index = packet_number * BUFFER_SIZE + m
-            image[index] = packet[m]
+            # 50 packets for 1 image
+            for packet_number in range(0, 50):
 
-        # Make sure all 9216 bytes have been received
-        while (total_bytes < BUFFER_SIZE):
-            packet = bytearray(BUFFER_SIZE - total_bytes)
-            bytes_recvd = s.recv_into(packet)
+                # Receive 9216 bytes of data from server
+                bytes_recvd = self.socket.recv_into(self.packet)
+                total_bytes = bytes_recvd
 
-            # TODO: optimize this, there's definitely a faster way to do this
-            # Copy image data from packet into the image buffer
-            for m in range(0, bytes_recvd):
-                index = packet_number * BUFFER_SIZE + total_bytes + m
-                image[index] = packet[m]
+                # TODO: optimize using list slicing
+                for m in range(0, bytes_recvd):
+                    index = packet_number * self.buffer_size + m
+                    self.image[index] = packet[m]
 
-            total_bytes += bytes_recvd
+                # Make sure all 9216 bytes have been received
+                while (total_bytes < self.buffer_size):
+                    packet = bytearray(self.buffer_size - total_bytes)
+                    bytes_recvd = self.socket.recv_into(self.packet)
 
-    # Write image to file
-    image_file = open('tmp.yuv', 'wb+')
-    image_file.write(image)
-    image_file.close()
+                    # TODO: optimize this, there's definitely a faster way to do this
+                    # Copy image data from packet into the image buffer
+                    for m in range(0, bytes_recvd):
+                        index = packet_number * self.buffer_size + total_bytes + m
+                        self.image[index] = packet[m]
 
-    # FFMPEG in silent mode to convert the image
-    ffmpeg_command = ['ffmpeg', '-s', '640x480', '-i', 'tmp.yuv', 'tmp.jpg', '-loglevel', 'panic']
+                    total_bytes += bytes_recvd
 
-    subprocess.call(ffmpeg_command, stdout=open(os.devnull, 'wb'))
+            # Write image to file
+            image_file = open('tmp.yuv', 'wb+')
+            image_file.write(self.image)
+            image_file.close()
 
-    # Now let opencv decompress your image
-    cvimage = cv.LoadImage("tmp.jpg")
+            # FFMPEG in silent mode to convert the image
+            ffmpeg_command = ['ffmpeg', '-s', '640x480', '-i', 'tmp.yuv', 'tmp.jpg', '-loglevel', 'panic']
 
-    # Put on computer display
-    cv.ShowImage('Camera', cvimage)
+            subprocess.call(ffmpeg_command, stdout=open(os.devnull, 'wb'))
 
-    # Escape kills the program
-    # NOTE: waitkey is crucial for refreshing the gui
-    if cv.WaitKey(10) == 27:
-        break
+            # Now let opencv decompress your image
+            cvimage = cv.LoadImage("tmp.jpg")
 
-    position = colortracker.get_position(cvimage, threshold=10000)
+            if self.display:
+                # Put on computer display
+                cv.ShowImage('Camera', cvimage)
 
-    if position:
-        print "Red found, position is " + str(position) + "!"
-        print "image %d received" % (image_number)
-        print "time is %d \n\n" % (int(round(time.time() * 1000))-START)
+                # Esc key kills the program, waitkey refreshes the display
+                if cv.WaitKey(10) == 27:
+                    break
+
+            position = self.colortracker.get_position(cvimage, threshold=self.threshold)
+
+            if position:
+                # Position found
+                print "Red found, position is " + str(position) + "!"
+                print "image %d received" % (image_number)
+                print "time is %d \n\n" % (int(round(time.time() * 1000)) - start_time)
+
+                # Assume the "value" is an angular displacement
+                ang_dspl = str(int(position))
+
+                # Send angular displacement back to the AR Drone
+                if (len(ang_dspl) != 4):
+                    ang_dspl = ang_dspl.zfill(4)
+                    bytes_sent = self.socket.send(ang_dspl)
+                else:
+                    bytes_sent = self.socket.send(ang_dspl)
+            else:
+                # No position found, send "None"
+                bytes_sent = s.send(str(None))
+
+            print "bytes_sent is %d" % (bytes_sent)
+
+            image_number += 1
+
+def cli():
+    # Command line interface
+
+    if len(sys.argv) < 2:
+        # No args: show usage info
+        print "Usage: " + sys.argv[0] + " [threshold] [--showdisplay]"
+        sys.exit()
+
+    elif len(sys.argv) == 2:
+        # Threshold, no image stream
+        threshold = int(sys.argv[1])
+        position_server = PositionServer(threshold=threshold)
+
+    elif len(sys.argv) == 3:
+        # Threshold, show image stream
+        threshold = sys.argv[1]
+        display = sys.argv[2] == "--showdisplay"
+
+        position_server = PositionServer(threshold=threshold, display=display)
 
 
-    	# Pretend Image Processing Algorithm is a float from -50 to 50
-    	ang_dspl = str(int(position))
+    position_server.connect()
+    print "Connected!"
 
-    	# Output Angle Displacement back to main_motorboard.c
-    	if (len(ang_dspl) != 4):
-            ang_dspl = ang_dspl.zfill(4)
-            bytes_sent = s.send(ang_dspl)
-    	else:
-            bytes_sent = s.send(ang_dspl)
+    print "Starting image streaming..."
+    position_server.main_loop()
 
-    else:
-	bytes_sent = s.send(str(None))
-
-
-    print "bytes_sent is %d" % (bytes_sent)
-
-    image_number += 1
-
-
-print "Execution complete."
+if __name__ == "__main__":
+    cli()
