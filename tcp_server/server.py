99c3d6f65382a93220390cca72da16a926bcee42
Sunny <sunnygautam009@gmail.com>
2013-06-03 10:29:33 -0400

    reverted server.py to correct version

--- tcp_server/server.py
+++ tcp_server/server.py
@@ -12,17 +12,16 @@ from colortracker import ColorTracker
 
 class PositionServer():
 
-    def __init__(self, ip='192.168.1.1', port=7777, buffer_size=9216, yuv_size=460800, display=False, threshold=10000, debug=True):
+    def __init__(self, ip='192.168.1.1', port=7777, buffer_size=9216, yuv_size=460800, display=False, threshold=10000):
         # Initialize the environment
         self.tcp_ip = ip
         self.tcp_port = port
         self.buffer_size = buffer_size
         self.yuv_size = yuv_size
-	self.debug = debug
 
         # Allocate memory for images and packets
         self.image = bytearray(self.yuv_size)
-        self.packet = bytearray(self.buffer_size)
+        self.packet = bytearray(self.yuv_size)
 
         # Instantiate color tracker
         self.colortracker = ColorTracker()
@@ -50,30 +49,27 @@ class PositionServer():
         while True:
             start_time = int(round(time.time() * 1000))
 
-            # 50 packets for 1 image
-            for packet_number in range(0, 50):
+            # Receive 460800 bytes of data from server
+            bytes_recvd = self.socket.recv_into(self.packet)
+            total_bytes = bytes_recvd
 
-                # Receive 9216 bytes of data from server
-                bytes_recvd = self.socket.recv_into(self.packet)
-                total_bytes = bytes_recvd
+            # TODO: optimize using list slicing
+            for m in range(0, bytes_recvd):
+                index = m
+                self.image[index] = self.packet[m]
 
-                # TODO: optimize using list slicing
+            # Make sure all 460800 bytes have been received
+            while (total_bytes < self.yuv_size):
+                packet = bytearray(self.yuv_size - total_bytes)
+                bytes_recvd = self.socket.recv_into(packet)
+
+                # TODO: optimize this, there's definitely a faster way to do this
+                # Copy image data from packet into the image buffer
                 for m in range(0, bytes_recvd):
-                    index = packet_number * self.buffer_size + m
-                    self.image[index] = self.packet[m]
+                    index = total_bytes + m
+                    self.image[index] = packet[m]
 
-                # Make sure all 9216 bytes have been received
-                while (total_bytes < self.buffer_size):
-                    packet = bytearray(self.buffer_size - total_bytes)
-                    bytes_recvd = self.socket.recv_into(packet)
-
-                    # TODO: optimize this, there's definitely a faster way to do this
-                    # Copy image data from packet into the image buffer
-                    for m in range(0, bytes_recvd):
-                        index = packet_number * self.buffer_size + total_bytes + m
-                        self.image[index] = packet[m]
-
-                    total_bytes += bytes_recvd
+                total_bytes += bytes_recvd
 
             # Write image to file
             image_file = open('tmp.yuv', 'wb+')
@@ -98,13 +94,11 @@ class PositionServer():
 
             position = self.colortracker.get_position(cvimage, threshold=self.threshold)
 
-	    if self.debug:
-            	print "image %d received" % (image_number)
-            	print "time is %d \n\n" % (int(round(time.time() * 1000)) - start_time)
-
+            print "time is %d \n\n" % (int(round(time.time() * 1000)) - start_time)
             if position:
                 # Position found
                 print "Red found, position is " + str(position) + "!"
+                print "image %d received" % (image_number)
 
                 # Assume the "value" is an angular displacement
                 ang_dspl = str(int(position))
@@ -118,6 +112,8 @@ class PositionServer():
             else:
                 # No position found, send "None"
                 bytes_sent = self.socket.send(str(None))
+
+            print "bytes_sent is %d" % (bytes_sent)
 
             image_number += 1
 
@@ -150,4 +146,3 @@ def cli():
 
 if __name__ == "__main__":
     cli()
-
